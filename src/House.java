8c4da88edd58ecf60121b51f2618358eb861da93
Scarlett Solórzano <ssolorzano0@ucol.mx>
2025-10-16 22:37:17 -0600

    Created user and house class

---
+++ src/House.java
@@ -0,0 +1,22 @@
+public class House {
+    
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,22 @@
+public class User{
+    private String name;
+    private String house;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHouse(){
+        return house;
+    }
+
+     public void setName(String name){
+        this.name = name;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+   
+}
